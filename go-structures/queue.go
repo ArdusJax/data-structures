6e6b31ac9deac579dcd9b87585bfc958db5fabaf
Chris O'Brian <mobiletread@gmail.com>
2023-07-07 17:21:52 -0700

    adding golang implementation of queue

---
+++ go-structures/queue.go
@@ -0,0 +1,62 @@
+// Package queue - queue is a package that contains the Queue datastructure and associated functions and methods
+package queue
+
+// Node - Node is a data type that is contained by the Queue
+type Node struct {
+	data int
+	next *Node
+}
+
+func newNode(data int) *Node {
+	return &Node{
+		data: data,
+		next: nil,
+	}
+}
+
+// Queue - Queue is a common data structure that is used in programming based on a linked list
+type Queue struct {
+	head   *Node
+	tail   *Node
+	length int
+}
+
+func newQueue() Queue {
+	return Queue{
+		head:   nil,
+		tail:   nil,
+		length: 0,
+	}
+}
+
+func (q *Queue) enqueue(data int) {
+	node := newNode(data)
+	if q.tail != nil {
+		q.tail.next = node
+		q.tail = node
+		q.length++
+	} else {
+		q.head = node
+		q.tail = node
+		q.length++
+	}
+}
+
+func (q *Queue) dequeue() int {
+	if q.head != nil {
+		data := q.head.data
+		q.head = q.head.next
+		q.length--
+		return data
+	}
+
+	return 0
+}
+
+func (q *Queue) peek() int {
+	if q.head != nil {
+		return q.head.data
+	}
+
+	return 0
+}
---
+++ go-structures/queue_test.go
@@ -0,0 +1,45 @@
+package queue
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidatorShouldSucceed(t *testing.T) {
+	for scenario, fn := range map[string]func(t *testing.T){
+		"successfully enqueued an int": enqueueIntIsSuccessful,
+		"successfully dequeued an int": dequeueIntIsSuccessful,
+		"successfully peek":            peekIntIsSuccessful,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			fn(t)
+		})
+	}
+}
+
+func enqueueIntIsSuccessful(t *testing.T) {
+	q := newQueue()
+	q.enqueue(32)
+	assert.Equal(t, 32, q.peek())
+}
+
+func dequeueIntIsSuccessful(t *testing.T) {
+	q := newQueue()
+	q.enqueue(12)
+	q.enqueue(32)
+	q.enqueue(22)
+	q.enqueue(42)
+	q.enqueue(52)
+	assert.Equal(t, 12, q.dequeue())
+	assert.Equal(t, 32, q.peek())
+}
+
+func peekIntIsSuccessful(t *testing.T) {
+	q := newQueue()
+	q.enqueue(42)
+	assert.Equal(t, 42, q.peek())
+
+	q.dequeue()
+	assert.Equal(t, 0, q.peek())
+}
